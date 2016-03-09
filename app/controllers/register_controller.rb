6b9ebad31ea428ea857842d08eb7e86420e7f7a6
LEARN <learn@LEARN12.local>
2016-03-09 14:58:16 -0800

    added logout button that resets the session

--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -4,39 +4,44 @@ class RegisterController < ApplicationController
   end
   #method to store user input into the database. If a field is empty, don't save an empty string into the database.
   def registeruser
-    user = User.new
-    user.first = params[:first_name] unless params[:first_name].strip.empty?
-    user.last = params[:last_name] unless params[:last_name].strip.empty?
-    user.street = params[:street] unless params[:street].strip.empty?
-    user.city = params[:city] unless params[:city].strip.empty?
-    user.state = params[:state] unless params[:state].strip.empty?
-    user.zip = params[:zip] unless params[:zip].strip.empty?
-    user.country = params[:country] unless params[:country].strip.empty?
-    user.email = params[:email] unless params[:email].strip.empty?
-    user.username = params[:username]
-    user.password = params[:password]
-    user.save
-    #Only add a phone number to the database if a number is entered.
-    if !params[:phone1].strip.empty?
-      number = Phone.new
-      number.phone = params[:phone1]
-      number.user_id = user.id
-      number.save
+    if !(User.find_by username: params[:username]).nil?
+      flash[:notice] = "Username taken, try another"
+      redirect_to "/"
+    else
+      user = User.new
+      user.first = params[:first_name] unless params[:first_name].strip.empty?
+      user.last = params[:last_name] unless params[:last_name].strip.empty?
+      user.street = params[:street] unless params[:street].strip.empty?
+      user.city = params[:city] unless params[:city].strip.empty?
+      user.state = params[:state] unless params[:state].strip.empty?
+      user.zip = params[:zip] unless params[:zip].strip.empty?
+      user.country = params[:country] unless params[:country].strip.empty?
+      user.email = params[:email] unless params[:email].strip.empty?
+      user.username = params[:username]
+      user.password = params[:password]
+      user.save
+      #Only add a phone number to the database if a number is entered.
+      if !params[:phone1].strip.empty?
+        phone = Phone.new
+        phone.phone = params[:phone1]
+        user.phones << phone
+        phone.save
+      end
+      if !params[:phone2].strip.empty?
+        phone = Phone.new
+        phone.phone = params[:phone2]
+        user.phones << phone
+        phone.save
+      end
+      if !params[:phone3].strip.empty?
+        phone = Phone.new
+        phone.phone = params[:phone3]
+        user.phones << phone
+        phone.save
+      end
+      #After registration is complete, user is taken to a confirmation page.
+      redirect_to '/register/confirmation'
     end
-    if !params[:phone2].strip.empty?
-      number = Phone.new
-      number.phone = params[:phone2]
-      number.user_id = user.id
-      number.save
-    end
-    if !params[:phone3].strip.empty?
-      number = Phone.new
-      number.phone = params[:phone3]
-      number.user_id = user.id
-      number.save
-    end
-    #After registration is complete, user is taken to a confirmation page.
-    redirect_to '/register/confirmation'
   end
 
   #Method that runs when the login page first loads
@@ -66,6 +71,11 @@ class RegisterController < ApplicationController
   def welcome
     @user = User.find_by id: session[:id]
     @phones = @user.phones
+  end
+
+  def logout
+    reset_session
+    redirect_to "/"
   end
 
 end
